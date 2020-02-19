fb06b72fdb45ef197cd3205bc0a33404b867d977
ymrabti <47449165+ymrabti@users.noreply.github.com>
2020-02-19 14:19:52 +0100

    gestionnaire utilisateurs

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GestionnaireUtilisateurs.Startup))]
+namespace GestionnaireUtilisateurs
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
