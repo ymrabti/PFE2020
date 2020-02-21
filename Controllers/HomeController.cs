c054180dcf3187f35d3f6c6c520fc5e70a9bb11b
ymrabti <47449165+ymrabti@users.noreply.github.com>
2020-02-21 14:47:49 +0100

    commit 20022020

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,10 +10,38 @@ namespace GestionnaireUtilisateurs.Controllers
     public class HomeController : Controller
     {
         aurs1Entities database = new aurs1Entities();
+        [Authorize]
         public ActionResult Index()
         {
             return View(database.AspNetUsers.ToList());
         }
+
+
+        [Authorize]
+        public ActionResult AddUser() {
+            return View();
+        }
+
+        [Authorize]
+
+        public ActionResult UserTache() {
+            return View();
+        }
+        [Authorize]
+        public ActionResult module() {
+            return View(database.Module.ToList());
+        }
+        [Authorize]
+        public ActionResult sousmodule() {
+            return View(database.SousModule.ToList());
+        }
+        [Authorize]
+        public ActionResult taches() {
+            return View(database.AspNetRoles.ToList());
+        }
+
+
+
 
         public ActionResult About()
         {
@@ -26,15 +54,6 @@ namespace GestionnaireUtilisateurs.Controllers
         {
             ViewBag.Message = "Your contact page.";
 
-            return View();
-        }
-
-        public ActionResult AddUser() {
-            return View();
-        }
-
-
-        public ActionResult UserTache() {
             return View();
         }
     }
