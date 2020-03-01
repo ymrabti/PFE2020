4c4f1ca446a5f650428abe9d750cf716c1e6dd7b
ymrabti <47449165+ymrabti@users.noreply.github.com>
2020-03-01 21:41:23 +0100

    commit 24022020

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -20,7 +22,8 @@ namespace GestionnaireUtilisateurs.Controllers
 
 
         [Authorize]
-        public ActionResult AddUser() {
+        public ActionResult AddUser()
+        {
             return View();
         }
 
@@ -33,29 +36,40 @@ namespace GestionnaireUtilisateurs.Controllers
         }
 
         [Authorize]
-        public ActionResult UserTache() {
-            String module_selected = Request.Form["moduleSelected"];
+        public ActionResult UserTache(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AspNetUsers aspNetUser = database.AspNetUsers.Find(id);
+            if (aspNetUser == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Nom = aspNetUser.Nom;
+            ViewBag.UserId = aspNetUser.Id;
+            ViewBag.Prenom = aspNetUser.Prenom;
+            ViewBag.Email = aspNetUser.Email;
+            ViewBag.StatusName = aspNetUser.Statuts.StatutName;
+
             var model = new MultiModeles
             {
-                aspNetUsers = database.AspNetUsers.Where(p => p.NomAr != null).ToList(),
+                aspNetUsers = database.AspNetUsers.Where(p => p.Id == id).ToList(),
                 aspNetRoles = database.AspNetRoles.ToList(),
-                aspNetUserRoles = database.AspNetUserRoles.ToList(),
+                aspNetUserRoles = database.AspNetUserRoles.Where(user => user.UserId == id).ToList(),
                 modules = database.Module.ToList(),
                 sousModules = database.SousModule.ToList()
             };
-            ViewBag.Users = new SelectList(database.AspNetUsers.Where(p => p.NomAr != null), "Id", "NomAr");
             ViewBag.modules = new SelectList(database.Module, "ModuleId", "ModuleName");
-            ViewBag.ModuleSelected = 2;
-
-
-
             ViewBag.sousmodules = new SelectList(database.SousModule, "SousModuleId", "SousModuleName");
             ViewBag.taches = new SelectList(database.AspNetRoles, "Id", "Name");
             return View(model);
         }
-        
+
         [Authorize]
-        public ViewResult StatutTache() {
+        public ViewResult StatutTache()
+        {
             var model = new MultiModeles
             {
                 statuts = database.Statuts.ToList(),
@@ -64,7 +78,7 @@ namespace GestionnaireUtilisateurs.Controllers
                 modules = database.Module.ToList(),
                 sousModules = database.SousModule.ToList()
             };
-           
+
             ViewBag.modules = new SelectList(database.Module, "ModuleId", "ModuleName");
             ViewBag.sousmodules = new SelectList(database.SousModule, "SousModuleId", "SousModuleName");
             ViewBag.taches = new SelectList(database.AspNetRoles, "Id", "Name");
@@ -75,15 +89,18 @@ namespace GestionnaireUtilisateurs.Controllers
 
 
         [Authorize]
-        public ActionResult module() {
+        public ActionResult module()
+        {
             return View(database.Module.ToList());
         }
         [Authorize]
-        public ActionResult sousmodule() {
+        public ActionResult sousmodule()
+        {
             return View(database.SousModule.ToList());
         }
         [Authorize]
-        public ActionResult taches() {
+        public ActionResult taches()
+        {
             return View(database.AspNetRoles.ToList());
         }
 
@@ -107,6 +124,21 @@ namespace GestionnaireUtilisateurs.Controllers
 
             return Json(getepassdata);
         }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult TableResult([Bind(Include = "Id,Name,SouModuleId,RoleDescription")] AspNetUserRoles aspNetRoles)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.tab = "modele non valide";
+            }
+            else
+            {
+                ViewBag.tab = aspNetRoles.ToString();
+            }
+
+            return View();
+        }
 
 
         public ActionResult About()
@@ -116,10 +148,59 @@ namespace GestionnaireUtilisateurs.Controllers
             return View(new AspNetUserRoles { });
         }
 
+
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Contacter([Bind(Include = "UserID,Username")] AspNetUserRoles user
+            , string answer, string[] Read,
+             string[] Create,
+             string[] Update,
+             string[] Delete, string[] UserId, string[] RoleId, string[] Lire)
+        {
+            ViewBag.User = "" + UserId.Length + " ::: ";
+            ViewBag.Tache = "" + RoleId.Length + " ::: ";
+            ViewBag.Create = "" + Create.Length + " ::: ";
+            ViewBag.Read = "" + Read.Length + " ::: ";
+            ViewBag.Update = "" + Read.Length + " ::: ";
+            ViewBag.Delete = "" + Read.Length + " ::: ";
+            ViewBag.Lire = "" + Lire.Length + " ::: ";
+            if (ModelState.IsValid && Read.Length != 0)
+            {
+                foreach (string element in UserId)
+                {
+                    ViewBag.User += element + " ***** ";
+                }
+                foreach (string element in RoleId)
+                {
+                    ViewBag.Tache += element + " ***** ";
+                }
+                foreach (string element in Create)
+                {
+                    ViewBag.Create += element + " ***** ";
+                }
+                foreach (string element in Read)
+                {
+                    ViewBag.Read += element + " ***** ";
+                }
+                foreach (string element in Update)
+                {
+                    ViewBag.Update += element + " ***** ";
+                }
+                foreach (string element in Delete)
+                {
+                    ViewBag.Delete += element + " ***** ";
+                }
+                foreach (string element in Lire)
+                {
+                    ViewBag.Lire += element + " ***** ";
+                }
+            }
             return View();
         }
     }
